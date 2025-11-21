5473eacbccb1bbb1710cb5cec7b5e856d980101a
César Rodríguez <cesaralejandrodriv@gmail.com>
2025-11-21 11:39:29 +0000

    Corrección, pasar a operador terciario

--- src/js/app.js
+++ src/js/app.js
@@ -28,41 +28,31 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  const getValue = (value, defaultValue = "") => {
-    return value === null || value === undefined || value === "null"
-      ? defaultValue
-      : value;
-  };
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${getValue(variables.name)} ${getValue(variables.lastName)}</h1>
-          <h2>${getValue(variables.role)}</h2>
-          <h3>${getValue(variables.city)}${
-    variables.city && variables.country ? ", " : ""
-  }${getValue(variables.country)}</h3>
+          <h1>${variables.name ? variables.name : "Jane"} ${
+    variables.lastName ? variables.lastName : "Doe"
+  }</h1>
+          <h2>${variables.role ? variables.role : ""}</h2>
+          <h3>${variables.city ? variables.city : ""}${
+    variables.country ? variables.country : ""
+  }</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy">${getValue(
-              variables.twitter
-            )}${getValue(
-    variables.twitter
-  )}<i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"${getValue(
-              variables.github
-            )}${getValue(
-    variables.github
-  )}<i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"${getValue(
-              variables.linkedin
-            )}">${getValue(
-    variables.linkedin
-  )}<i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy">${getValue(
-              variables.instagram
-            )}${getValue(
-    variables.instagram
-  )}<i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/4geeksacademy">${
+              variables.twitter ? variables.twitter : ""
+            }<i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/4geeksacademy"${
+              variables.github ? variables.github : ""
+            }<i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/4geeksacademy"${
+              variables.linkedin ? variables.linkedin : ""
+            }<i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/4geeksacademy">${
+              variables.instagram ? variables.instagram : ""
+            }<i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
